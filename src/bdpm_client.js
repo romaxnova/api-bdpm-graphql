8abd2a18878ac289ec09c7ad759c779495b6875b
romaxnova <125314160+romaxnova@users.noreply.github.com>
2025-08-09 10:18:50 +0200

    Update bdpm_client.js to use correct url (changed)

--- src/bdpm_client.js
+++ src/bdpm_client.js
@@ -10,7 +10,7 @@ function downloadFile(filename, {
     path = process.env.BDPM_URL_PATH || "/telechargement.php"
 } = {}) {
     return new Promise((resolve, reject) => {
-        const url = new URL(`${protocol}//${host}${path}?fichier=${filename}.txt`);
+        const url = new URL(`${protocol}//${host}${path}/${filename}.txt`);
         const timer = `Downloaded ${filename}`;
         console.time(timer);
         const req = https.request(url, res => {
